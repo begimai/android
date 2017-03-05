0bee823c9ade02e96c046bad2474238193c00d27
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-03-05 19:45:10 +0600

    my first Hello world app
    As it is common to create a "Hello World" program for any programming
    languages, here is my first android app, which receives your name. When
    you press a button, it prints "Hi" + your name. There is an image of
    android on the buttom

---
+++ helloWorld/app/src/main/java/com/example/asus/helloworld/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.asus.helloworld;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText nameEditText;
+    private TextView messageEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main); // when activity main is created, open, analyze
+                                                // "inflate?"
+        nameEditText = (EditText) findViewById(R.id.nameEditText);
+        messageEditText = (TextView) findViewById(R.id.messageEditText);
+    }
+
+    public void great(View view) {
+        String name = nameEditText.getText().toString();
+        messageEditText.setText(getString(R.string.greetingMessage, name));
+    }
+}

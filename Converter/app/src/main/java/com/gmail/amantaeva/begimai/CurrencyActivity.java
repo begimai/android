d29590b408638b4db8d6eee5b7566686c816ec84
Begimai Amantaeva <begimai.amantaeva@gmail.com>
2017-03-10 14:31:22 +0600

    created a json file for currency converter

--- Converter/app/src/main/java/com/gmail/amantaeva/begimai/CurrencyActivity.java
+++ Converter/app/src/main/java/com/gmail/amantaeva/begimai/CurrencyActivity.java
@@ -2,15 +2,34 @@ package com.gmail.amantaeva.begimai;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 import com.example.amantaeva.begimai.converter.R;
 
+import org.json.JSONObject;
+
 public class CurrencyActivity extends AppCompatActivity {
+    private EditText firstCurrencyEditText;
+    private EditText secondCurrencyEditText;
+    private EditText customConversionRatioEditText;
+    private TextWatcher customConversionRatioEditTextTextWatcher;
+
+    private Spinner firstCurrencySpinner;
+    private Spinner secondCurrencySpinner;
+
+    private JSONObject conversionRatios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_currency);
+
     }
+
     //json
 }
